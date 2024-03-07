3e3a59c7ccc1d8ff2c53272c4880e962816123e4
Etwashoeren <sungmin0609@nate.com>
2024-03-07 16:53:07 +0900

    update

--- pyburger/config/urls.py
+++ pyburger/config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from config.views import main # views.py에 작성한 main 함수를 가져오기
+
+# burger_list 함수를 추가적으로 import
+from config.views import main, burger_list
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", main), # 공백(아무것도 입력하지 않은 경로)과 main 함수를 연결
+    path("burgers/", burger_list),
 ]
--- pyburger/config/views.py
+++ pyburger/config/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def main(request):
-    return HttpResponse("안녕하세요, pyburger입니다.")
+    return render(request, 'main.html')
+
+def burger_list(request):
+    return render(request, 'burger_list.html')
