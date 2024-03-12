316befe5d4fe1e137a332f1f8949f116cacd015a
Etwashoeren <sungmin0609@nate.com>
2024-03-12 15:14:07 +0900

    update

--- pyburger/config/views.py
+++ pyburger/config/views.py
@@ -16,8 +16,11 @@ def burger_list(request):
 def burger_search(request):
     keyword =request.GET.get('keyword')
 
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
 
-    burgers = Burger.objects.filter(name__contains=keyword)
+    else:
+        burgers = Burger.objects.none()
 
     context = {
         "burgers": burgers,
