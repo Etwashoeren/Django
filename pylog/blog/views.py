52cb31673f855a9c6a76eb645f1001b083030f35
Etwashoeren <sungmin0609@nate.com>
2024-03-17 19:47:51 +0900

    update

--- pylog/blog/views.py
+++ pylog/blog/views.py
@@ -8,3 +8,13 @@ def post_list(request):
     context = {'posts': posts}
 
     return render(request, 'post_list.html', context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id) # id값이 URL에서 받은 post_id값인 Post 객체
+    print(post)                         # 가져온 객체를 print 함수로 확인
+
+    context = {
+        # post_id 대신 Post 객체를 전달
+        "post":post,
+    }
+    return render(request, 'post_detail.html', context)
